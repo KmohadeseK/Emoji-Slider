47f8fdc9812e5588d420bbd711be4ea36f3ea7bd
Mohadese2006 <mohadesekeshtkar4@gmail.com>
2023-06-21 09:17:31 -0700

    The first commit of this project

---
+++ Purple Project/script.js
@@ -0,0 +1,38 @@
+var emoji = document.getElementById('emoji');
+var mood = document.getElementById('mood');
+var sliderValue = document.getElementById('range');
+
+sliderValue.oninput = function()
+{
+var value = this.value;
+if(value <=0)
+{
+    emoji.innerHTML ='💜';
+    mood.innerHTML ='Heart';
+}
+
+else if(value <=40)
+{
+    emoji.innerHTML ='🍇';
+    mood.innerHTML ='Grape';
+}
+
+else if(value <=60)
+{
+    emoji.innerHTML ='🔮';
+    mood.innerHTML ='Orb';
+}
+
+else if(value <=80)
+{
+    emoji.innerHTML ='👾';
+    mood.innerHTML ='Alien Monster';
+}
+
+else if(value <=100)
+{
+    emoji.innerHTML ='💁‍♀️';
+    mood.innerHTML ='Thanks for watching';
+}
+
+}
