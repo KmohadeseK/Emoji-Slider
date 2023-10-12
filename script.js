dcb44d673f9a1725d79e6b8ce950bbf01f8c23c6
Mohadese-Keshtkar <mohadesekeshtkar4@gmail.com>
2023-10-12 10:46:31 +0330

    update js slids and css font, background

--- script.js
+++ script.js
@@ -5,32 +5,32 @@ var sliderValue = document.getElementById('range');
 sliderValue.oninput = function () {
     var value = this.value;
     if (value <= 0) {
-        emoji.innerHTML = '💜';
-        mood.innerHTML = 'Heart';
+        emoji.innerHTML = '🥰';
+        mood.innerHTML = 'Lovely';
     }
 
     else if (value <= 20) {
-        emoji.innerHTML = '🍇';
-        mood.innerHTML = 'Grape';
+        emoji.innerHTML = '😭';
+        mood.innerHTML = 'Cry';
     }
 
     else if (value <= 40) {
-        emoji.innerHTML = '🔮';
-        mood.innerHTML = 'Orb';
+        emoji.innerHTML = '🥲';
+        mood.innerHTML = 'Fear';
     }
 
     else if (value <= 60) {
-        emoji.innerHTML = '👾';
-        mood.innerHTML = 'Alien Monster';
+        emoji.innerHTML = '🥳';
+        mood.innerHTML = 'Happy';
     }
 
     else if (value <= 80) {
-        emoji.innerHTML = '🍆';
-        mood.innerHTML = 'Eggplant';
+        emoji.innerHTML = '🥺';
+        mood.innerHTML = 'Oppressed';
     }
 
     else if (value <= 100) {
-        emoji.innerHTML = '💁‍♀️';
+        emoji.innerHTML = '💜';
         mood.innerHTML = 'Thanks for watching';
     }
 
